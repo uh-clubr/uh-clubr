74ba694d4b817bca5037c2974f935aaa3d714e71
Andrea Narciso <anarciso@middlebury.com>
2018-11-16 14:25:00 -1000

    Issue-18: Changed alignment of Club Description

--- app/imports/ui/components/Club.jsx
+++ app/imports/ui/components/Club.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Image, Icon, Container, Header } from 'semantic-ui-react';
+import { Card, Image, Icon, Header, Grid } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 
@@ -7,37 +7,42 @@ import { withRouter } from 'react-router-dom';
 class Club extends React.Component {
   render() {
     return (
-      <Card fluid>
+        <Card fluid>
           <Card.Content floated='left'>
-            <Image floated='left' rounded size='medium' src={this.props.club.image} />
-            <Container>
-              <Header
-                  as='h2'
-                  content={this.props.club.name}
-                  subheader={this.props.club.type}
-              />
-            </Container>
-            <Card.Description>{this.props.club.description}</Card.Description>
+            <Image floated='left' rounded size='medium' src={this.props.club.image}/>
+            <Grid>
+              <Grid.Row>
+                <Header
+                    as='h2'
+                    floated='left'
+                    content={this.props.club.name}
+                    subheader={this.props.club.type}
+                />
+              </Grid.Row>
+              <Grid.Row>
+                <Card.Description>{this.props.club.description}</Card.Description>
+              </Grid.Row>
+            </Grid>
           </Card.Content>
           <Card.Content extra>
             <a href={this.props.club.rio_website}>
-              <Icon name='desktop' />
+              <Icon name='desktop'/>
             </a>
             <a href={this.props.club.rio_email}>
-            <Icon name='envelope outline' />
-          </a>
+              <Icon name='envelope outline'/>
+            </a>
             <a href={this.props.club.contact_email}>
-              <Icon name='address card outline' />
+              <Icon name='address card outline'/>
               {this.props.club.contact_person}
             </a>
             <a href={this.props.club.rio_facebook}>
-              <Icon name='facebook f' />
+              <Icon name='facebook f'/>
             </a>
             <a href={this.props.club.rio_instagram}>
-              <Icon name='instagram' />
+              <Icon name='instagram'/>
             </a>
             <a href={this.props.club.rio_twitter}>
-              <Icon name='twitter' />
+              <Icon name='twitter'/>
             </a>
           </Card.Content>
         </Card>
