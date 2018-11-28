f074eba16bd3271ac609dcff59c2b9de1d3ebe22
Andrea Narciso <anarciso@middlebury.com>
2018-11-27 17:23:02 -1000

    Updated CSS styling of card and club info links

--- app/imports/ui/components/Club.jsx
+++ app/imports/ui/components/Club.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Image, Icon, Header, Grid, Container, Modal, Button } from 'semantic-ui-react';
+import { Card, Image, Icon, Header, Grid, Container, Modal, Button, Popup } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 
@@ -8,18 +8,21 @@ class Club extends React.Component {
   render() {
     const cardDescStyle = { color: 'rgba(0, 0, 0, 0.87)', paddingTop: '10px' };
     const descStyle = { color: 'rgba(0, 0, 0, 0.87)' };
-    const modalContent = { paddingLeft: '0px' };
+    const clubCard = { paddingBottom: '0px' };
+    const modalButton = { padding: '0px', marginTop: '10px' };
+    const modalContent = { padding: '0px' };
+    const modalInfo = { padding: '14px' };
     return (
         <Card fluid>
           <Card.Content floated='left'>
             <Grid stackable columns={2}>
-              <Grid.Row>
-                <Grid.Column width={5}>
-                  <Image floated='left' rounded size='large' src={this.props.club.image}/>
-                </Grid.Column>
-                <Grid.Column width={11} stretched>
-                  <Grid.Row>
-                    <Header
+              <Grid.Row style={clubCard}>
+                      <Grid.Column width={5}>
+                        <Image floated='left' rounded size='large' src={this.props.club.image}/>
+                      </Grid.Column>
+                      <Grid.Column width={11} stretched>
+                        <Grid.Row>
+                          <Header
                         as='h2'
                         floated='left'
                         content={this.props.club.name}
@@ -27,23 +30,26 @@ class Club extends React.Component {
                     />
                     <Container textAlign='right'>
                       <a href={this.props.club.rio_website}>
-                        <Icon name='desktop'/>
+                        <Icon name='desktop' color='grey'/>
                       </a>
                       <a href={this.props.club.rio_email}>
-                        <Icon name='envelope outline'/>
+                        <Icon name='envelope outline' color='grey'/>
                       </a>
-                      <a href={this.props.club.contact_email}>
-                        <Icon name='address card outline'/>
-                        {this.props.club.contact_person}
+                      <a href={this.props.club.contact_email} color='grey'>
+                        <Popup
+                            trigger={<Icon name='address card outline' color='grey'/>}
+                            size='tiny'>
+                          {this.props.club.contact_person}
+                        </Popup>
                       </a>
                       <a href={this.props.club.rio_facebook}>
-                        <Icon name='facebook'/>
+                        <Icon name='facebook' color='grey'/>
                       </a>
                       <a href={this.props.club.rio_instagram}>
-                        <Icon name='instagram'/>
+                        <Icon name='instagram' color='grey'/>
                       </a>
                       <a href={this.props.club.rio_twitter}>
-                        <Icon name='twitter'/>
+                        <Icon name='twitter' color='grey'/>
                       </a>
                     </Container>
                   </Grid.Row>
@@ -54,30 +60,33 @@ class Club extends React.Component {
                   </Grid.Row>
                 </Grid.Column>
                 <Modal trigger={
-                  <Button icon compact fluid attached='bottom' size='mini'>
+                  <Button icon compact fluid attached='bottom' size='mini' style={modalButton}>
                     <Icon name='angle down'/>
                   </Button>
                 }>
                   <Modal.Header>{this.props.club.name}</Modal.Header>
-                  <Modal.Content scrolling>
+                  <Modal.Content scrolling style={modalContent}>
                     <Image src={this.props.club.image} fluid/>
-                    <Modal.Description className={modalContent}>
-                      <Header>About Us</Header>
+                    <Modal.Description style={modalInfo}>
+                      <Header>We are...</Header>
                       <p style={descStyle}>
                         {this.props.club.description}
                       </p>
                     </Modal.Description>
-                    <Modal.Description>
-                      <Header>We would get a long if you...</Header>
+                    <Image src={this.props.club.image} fluid/>
+                    <Modal.Description style={modalInfo}>
+                      <Header>We would get along if you...</Header>
                       <p style={descStyle}>
                         ANSWER HERE
                       </p>
                     </Modal.Description>
-                    <Modal.Description>
-                      <Header>Modal Header</Header>
+                    <Image src={this.props.club.image} fluid/>
+                    <Modal.Description style={modalInfo}>
+                      <Header>Meet us!</Header>
                       <p style={descStyle}>
-                        {this.props.club.description}
-                        2nd
+                        Example:
+                        M 3:30-4:00
+                        T 4:30-5:00
                       </p>
                     </Modal.Description>
                   </Modal.Content>
