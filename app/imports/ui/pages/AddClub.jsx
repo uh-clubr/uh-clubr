95e6ee8c27240480fca664fd2ff61c0f840400ed
Sophia Kim <sophiak8@hawaii.edu>
2018-12-04 19:14:41 -1000

    Update Add and edit page to include extra information.

--- app/imports/ui/pages/AddClub.jsx
+++ app/imports/ui/pages/AddClub.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Clubs, ClubSchema } from '/imports/api/club/club';
-import { Grid, Header, Segment, Step, Button } from 'semantic-ui-react';
+import { Grid, Message, Segment, Step, Icon, Header } from 'semantic-ui-react';
 import AutoForm from 'uniforms-semantic/AutoForm';
 import TextField from 'uniforms-semantic/TextField';
 import LongTextField from 'uniforms-semantic/LongTextField';
 import HiddenField from 'uniforms-semantic/HiddenField';
 import SelectField from 'uniforms-semantic/SelectField';
+import SubmitField from 'uniforms-semantic/SubmitField';
 import ErrorsField from 'uniforms-semantic/ErrorsField';
 import { Bert } from 'meteor/themeteorchef:bert';
 import { Meteor } from 'meteor/meteor';
-import { Link } from 'react-router-dom';
 
 /** Renders the Page for adding a document. */
 class AddClub extends React.Component {
@@ -35,41 +35,20 @@ class AddClub extends React.Component {
   /** On submit, insert the data. */
   submit(data) {
     const { name, type, contact_person, contact_email, rio_email, rio_website, rio_facebook, rio_instagram,
-      rio_twitter, image, image2, image3, description } = data;
+      rio_twitter, image, description } = data;
     const owner = Meteor.user().username;
     Clubs.insert({ name, type, contact_person, contact_email, rio_email, rio_website, rio_facebook, rio_instagram,
-      rio_twitter, image, image2, image3, description, owner }, this.insertCallback);
+      rio_twitter, image, description, owner }, this.insertCallback);
   }
 
   /** Render the form. Use Uniforms: https://github.com/vazco/uniforms */
   render() {
-    const stepStyle = {
-      paddingTop: '30px',
-      marginLeft: '200px',
-      marginBottom: '20px',
-    };
     return (
         <div className='landing-background'>
-          <div style={stepStyle}>
-            <Step.Group ordered>
-              <Step active>
-                <Step.Content>
-                  <Step.Title>What best describes you?</Step.Title>
-                  <Step.Description>Add fun questions for possible future clubrs to see!</Step.Description>
-                </Step.Content>
-              </Step>
-
-              <Step disabled>
-                <Step.Content>
-                  <Step.Title>Add Necessary Information</Step.Title>
-                </Step.Content>
-              </Step>
-            </Step.Group>
-          </div>
         <Grid container centered>
           <Grid.Column>
-            <Header as="h2" textAlign="center" className='header-style'>Add Club</Header>
-            <AutoForm ref={(ref) => { this.formRef = ref; }} schema={ClubSchema}>
+            <Header as="h2" textAlign="center" className='header-style'>Add a New Club!</Header>
+            <AutoForm ref={(ref) => { this.formRef = ref; }} schema={ClubSchema} onSubmit={this.submit}>
               <Segment>
                 <TextField name='name'/>
                 <SelectField name='type'/>
@@ -81,14 +60,16 @@ class AddClub extends React.Component {
                 <TextField name='rio_instagram'/>
                 <TextField name='rio_twitter'/>
                 <TextField name='image'/>
-                <TextField name='image2'/>
-                <TextField name='image3'/>
                 <LongTextField name='description'/>
+                <SubmitField value='Submit'/>
                 <ErrorsField/>
                 <HiddenField name='owner' value='fakeuser@foo.com'/>
-                <Button onSubmit={this.submit}><Link to={'/addclub2'}>Next</Link></Button>
               </Segment>
             </AutoForm>
+            <Message attached='bottom' warning>
+              <Icon name='info' />
+              To get noticed by future clubrs, go to the Edit page later to add more information!
+            </Message>
           </Grid.Column>
         </Grid>
       </div>
